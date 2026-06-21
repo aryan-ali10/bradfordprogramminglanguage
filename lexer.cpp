c3ebf8fdaef8dd06c4d7ec0439eca152962d2f9e
aryan-ali10 <aryan-ali10@hotmail.com>
2026-06-21 20:39:07 +0100

    feat: implement literal string, number, and word lexing

--- lexer.cpp
+++ lexer.cpp
@@ -65,3 +65,109 @@ void Lexer::skipWhitespaceAndComments()
     }
 
 }
+
+std::string Lexer::readWord()
+{
+    std::string word;
+    while (!isAtEnd() && (isalnum(peek()) || peek() == '_'))
+    {
+        word += advance();
+    }
+    return word;
+}
+
+ Token Lexer::makeNumber()
+ {
+    std::string numStr;
+    int startLine = line;
+    while (!isAtEnd() && (isdigit(peek())))
+    {
+        numStr += advance();
+    }
+    if (peek() == '.' && isdigit(peekNext()))
+    {
+        numStr += advance();
+        while (!isAtEnd() && isdigit(peek()))
+        {
+            numStr += advance();
+        }
+    }
+    Token t(TOK_NUMBER, numStr, startLine);
+    t.numberValue = std::stod(numStr);
+    return t;
+
+ }
+
+ Token Lexer::makeString()
+ {
+    int startLine = line;
+    advance();
+    std::string s;
+    while (!isAtEnd() && peek() != '"')
+    {
+        s += advance ();
+    }
+    if (isAtEnd()) advance;
+    return Token(TOK_STRING, s, startLine);
+ }
+
+ Token Lexer::tryMultiWord(const std::string & firstWord)
+ {
+    int startLine = line;
+    size_t savedPos = pos;
+    int savedLine = line;
+
+    // skip spaces
+    size_t lookPos = pos;
+    while (lookPos <src.size() && (src[lookPos] == ' ' || src [lookPos] == '\t')) 
+    {
+        lookPos++;
+    }
+    size_t wordStart = lookPos;
+    while (lookPos < src.size() && (isalnum(src[lookPos]) || src[lookPos] == '_'))
+    {
+        lookPos++;
+    }
+    std::string secondWord = src.substr(wordStart, lookPos - wordStart);
+    
+    // check for token combinations
+    if (firstWord == "wagwan" && secondWord == "cuz")
+    {
+        pos = lookPos;
+        return Token(TOK_WAGWANCUZ, "wagwan cuz", startLine);
+    }
+
+    if (firstWord == "send" && secondWord == "it")
+    {
+        pos = lookPos;
+        return Token(TOK_SENDIT, "send it", startLine);
+    }
+
+    if (firstWord == "innit" && secondWord == "yara")
+    {
+        pos = lookPos;
+        return Token(TOK_INNITYARA, "innit yara", startLine);
+
+    }
+
+    if (firstWord == "rev" && secondWord == "it")
+    {
+        pos = lookPos;
+        return Token(TOK_REVIT, "rev it", startLine);
+    }
+
+    if (firstWord == "top" && secondWord == "man")
+    {
+        pos = lookPos;
+        return Token(TOK_TOPMAN, "top man", startLine);
+    }
+
+    // If no combinations are found then treat it as a single word token
+    pos = savedPos;
+    line = savedLine;
+    Token none;
+    none.type = TOK_EOF;
+    return none;
+
+ }
+
