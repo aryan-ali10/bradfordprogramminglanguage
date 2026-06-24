3eef3a0e60cc25f653941740e5880656cb0fb44d
aryan-ali10 <aryan-ali10@hotmail.com>
2026-06-24 19:07:52 +0100

    feat: add main file to run the interpreter

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "lexer.h"
+#include "parser.h"
+#include "interpreter.h"
+
+std::string readFile(const std::string & path)
+{
+    std::ifstream file(path);
+    if (!file.is_open())
+    {
+        std::cerr << "kasme yara, I couldn't find the file: " << path << "\n";
+        exit(1);
+    }
+    std::stringstream ss;
+    ss <<file.rdbuf();
+    return ss.str();
+}
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        std::cerr << "usage: " << argv[0] << "<yourscript.brad>\n";
+        return 1;
+    }
+    std::string source = readFile(argv[1]);
+
+    Lexer lexer(source);
+    std::vector<Token> tokens = lexer.tokenise();
+
+    Parser parser(tokens);
+    Node* program = parser.parseProgram();
+
+    Interpreter interpret;
+    interpret.run(program);
+
+    return 0;
+}
